0444a6fbe316a1299d843a86686a7fbbf037f46f
colletemine <jepngetichcolleta@gmail.com>
2022-04-16 15:25:15 +0300

    generate a interface class

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+BASE_URL: string = 'https://api.github.com/'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getUsers():Observable<CanvasUserInterface>{
+
+  }
+  getRepository(){
+
+  }
 }
