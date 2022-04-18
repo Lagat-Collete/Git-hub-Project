9abc0b923066c3773d95fe522f67867efeb60fc3
colletemine <jepngetichcolleta@gmail.com>
2022-04-18 20:05:17 +0300

    add a form

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -9,7 +9,9 @@ import 'rxjs'
 })
 export class UsersService {
   [x: string]: any;
-private username:any = 'colletemine'
+private username:any = 'colletemine';
+private client_id = 'a81d5fc03398488b7ee1';
+private  client_secret = 'a992a6e1b5bc4955585b8944ca9f59b01c21cc0c'
 
 
   constructor(private _http:HttpClient) {
@@ -17,12 +19,12 @@ private username:any = 'colletemine'
    }
 
 getUser(){
-  return this._http.get('https://api.github.com/users/'+this.username)
+  return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
 
 }
 
 getRepos(){
-  return this._http.get('https://api.github.com/users/'+this.username+'/repos')
+  return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
 
 }
 updateUsername(username:any){
