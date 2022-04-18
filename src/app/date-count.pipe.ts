518a4b52eb68cd4f4693ce338e4cab952a59306f
colletemine <jepngetichcolleta@gmail.com>
2022-04-18 23:38:29 +0300

    add overflow to card

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -4,20 +4,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
-
   transform(value: any): number {
-    let today: Date = new Date(); //get current date and time
-    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(todayWithNoTime - value) //returns value in miliseconds
-    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-    var dateDifferenceSeconds = dateDifference * 0.001; //converts miliseconds to seconds
+    let today: Date = new Date(); // get the current date and time
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    var dateDifference = Math.abs(todayWithNoTime - value); //returns value in miliseconds
+    const secondsInDay = 86400; // 60seconds * 60 minutes in an hour * 24 hours in a day
+    
+    var dateDifferenceSeconds = dateDifference * 0.001; //convert miliseconds to seconds
     var dateCounter = dateDifferenceSeconds / secondsInDay;
-
-    if (dateCounter >= 1 && value) {
-      return dateCounter;
+    
+    if (dateCounter >= 1) {
+      return Math.round(dateCounter);
     } else {
       return 0;
     }
   }
-
 }
